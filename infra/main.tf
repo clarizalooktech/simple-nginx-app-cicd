1ef0b13dea356d56646a6b3313a83ba4c30acf7c
Clari Loo <clarizlook.tech@gmail.com>
2025-01-18 23:12:58 +0800

    Adding security group for ec2 in terraform

--- infra/main.tf
+++ infra/main.tf
@@ -2,12 +2,39 @@ provider "aws" {
   region = "ap-southeast-2"
 }
 
+variable "home_ip" {
+  description = " SSH access"
+  type        = string
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${var.home_ip}/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "app_server" {
   ami           = "ami-0d11f9bfe33cfbe8b" # free tier Amazon Linux 2023 AMI
   instance_type = "t2.micro"
+  key_name      = "rsakey"
+
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
   tags = {
-    Name = "NginxAppServer"
+    Name = "AppServer"
   }
 
   user_data = <<-EOF
