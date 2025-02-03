f0b6d225c933d99eddbb6ed1e1531cf572e2f32e
Clari Loo <clarizlook.tech@gmail.com>
2025-02-03 23:29:49 +0800

    Updated app.py for right naming of stack

--- infra/infrastructure_stack/infra_stack.py
+++ infra/infrastructure_stack/infra_stack.py
@@ -4,13 +4,13 @@ from aws_cdk import (
 )
 from constructs import Construct
 
-class InfraStack(Stack):
+class NginxCicdStack(Stack):
 
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # Define the VPC
-        vpc = ec2.Vpc(self, "MyVpc", max_azs=2)
+         # Use the default VPC
+        vpc = ec2.Vpc.from_lookup(self, "DefaultVpc", is_default=True)
 
         # Define the security group
         security_group = ec2.SecurityGroup(self, "SecurityGroup",
@@ -18,11 +18,12 @@ class InfraStack(Stack):
             description="Allow ssh access from the world",
             allow_all_outbound=True
         )
-        security_group.add_ingress_rule(ec2.Peer.ipv4("0.0.0.0/0"), ec2.Port.tcp(22), "Allow SSH access")
+        security_group.add_ingress_rule(ec2.Peer.ipv4("0.0.0.0/0"), 
+            ec2.Port.tcp(22), "Allow SSH access")
 
         # Define the EC2 instance
         ec2_instance = ec2.Instance(self, "Instance",
-            instance_type=ec2.InstanceType("t2.micro"),
+            instance_type=ec2.InstanceType("t2.micro"),  # Free Tier eligible instance type
             machine_image=ec2.MachineImage.latest_amazon_linux2(),
             vpc=vpc,
             security_group=security_group,
