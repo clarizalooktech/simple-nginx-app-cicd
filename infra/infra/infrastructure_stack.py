fe4bd5b0326b559249c2705241cb9814c9a56c7a
Clariza Look <clariza.look@navitas.com>
2025-02-26 13:11:55 +0800

    Adding 30 sec delay to wait for Ec2 instance to be ready

--- infra/infra/infrastructure_stack.py
+++ infra/infra/infrastructure_stack.py
@@ -2,7 +2,8 @@ from aws_cdk import (
     Stack,
     aws_ec2 as ec2,
     aws_iam as iam,
-    Tags
+    Tags,
+    CfnOutput
 )
 from constructs import Construct
 
@@ -27,33 +28,50 @@ class NginxCicdStack(Stack):
         instance_role = iam.Role(self, "InstanceRole",
             assumed_by=iam.ServicePrincipal("ec2.amazonaws.com")
         )
-        
+
         # Add ECR policy to the role
         instance_role.add_managed_policy(
             iam.ManagedPolicy.from_aws_managed_policy_name("AmazonEC2ContainerRegistryFullAccess")
         )
-        
+
         # Define the EC2 instance with the custom role
         ec2_instance = ec2.Instance(self, "Instance",
-            instance_type=ec2.InstanceType("t2.micro"),  
+            instance_type=ec2.InstanceType("t2.micro"),
             machine_image=ec2.MachineImage.latest_amazon_linux2(),
             vpc=vpc,
             security_group=security_group,
             key_name="rsakey",
-            role=instance_role  # Assign the role with ECR permissions
-        )
-        
-        # Add user data to install Docker and Nginx
-        ec2_instance.add_user_data(
-            "yum update -y",
-            "amazon-linux-extras install docker -y",
-            "service docker start",
-            "usermod -a -G docker ec2-user",
-            "systemctl enable docker",
-            "amazon-linux-extras install nginx1.12 -y",
-            "systemctl start nginx",
-            "systemctl enable nginx"
+            role=instance_role,  # Assign the role with ECR permissions
+            user_data=ec2.UserData.custom(self._get_user_data())
         )
 
         # Add a Name Tag to EC2 instance
         Tags.of(ec2_instance).add("Name", "NginxInstance")
+
+        # Output the instance ID and public IP
+        CfnOutput(self, "InstanceId", value=ec2_instance.instance_id)
+        CfnOutput(self, "InstancePublicIp", value=ec2_instance.instance_public_ip)
+
+    def _get_user_data(self):
+        return """#!/bin/bash
+# Update system packages
+yum update -y
+
+# Install Docker
+amazon-linux-extras install docker -y
+systemctl start docker
+systemctl enable docker
+usermod -a -G docker ec2-user
+
+# Install AWS CLI v2 if needed
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip -q awscliv2.zip
+./aws/install --update
+rm -rf aws awscliv2.zip
+
+# Create a status file to signal instance is ready
+touch /tmp/instance_ready
+
+# Log completion
+echo "Instance setup complete"
+"""
